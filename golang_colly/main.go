5cbe15376e300a1db76cd4210df8ca0a9cefc79d
Arlind <arlind12344@gmail.com>
2023-11-15 10:37:03 +0100

    golang colly testing

---
+++ golang_colly/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.ikea.com"),
+	)
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	c.Visit("https://www.ikea.com")
+}
