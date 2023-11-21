c52447781f09223e889d81d18ab4cd646ff2099f
Arlind <arlind12344@gmail.com>
2023-11-21 15:31:39 +0100

    fixed LoadFont

--- golang_colly_reddit/video/videoUtilities/videoUtilities.go
+++ golang_colly_reddit/video/videoUtilities/videoUtilities.go
@@ -1,11 +1,15 @@
 package videoUtilities
 
 import (
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 
-	"github.com/golang/freetype"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
+	"golang.org/x/image/font/sfnt"
 )
 
 func IsDir(path string) bool {
@@ -32,14 +36,35 @@ func EmptyDir(path string) error {
 	return nil
 }
 
-func LoadFont(path string) (*freetype.Font, error) {
+func LoadFont(path string) (font.Face, error) {
+	// Check if the path is a valid file
+	if fileInfo, err := os.Stat(path); err != nil {
+		return nil, fmt.Errorf("failed to access font file: %v", err)
+	} else if fileInfo.IsDir() {
+		return nil, errors.New("provided path is a directory, not a font file")
+	}
+
+	// Read the font file
 	fontBytes, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read font file: %v", err)
 	}
-	font, err := freetype.ParseFont(fontBytes)
+
+	// Parse the font data
+	f, err := sfnt.Parse(fontBytes)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to parse font data: %v", err)
 	}
-	return font, nil
+
+	// Create a font.Face from the parsed font
+	face, err := opentype.NewFace(f, &opentype.FaceOptions{
+		Size:    12, // Adjust the size based on your requirements
+		DPI:     72,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create font face: %v", err)
+	}
+
+	return face, nil
 }
